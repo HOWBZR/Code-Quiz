76d9e2b6532802089ab71702d06b819105a49aa9
David Coons <dlc9005@gmail.com>
2019-12-11 22:15:54 -0700

    added event listeners for buttons and itterators for score

--- script.js
+++ script.js
@@ -1,89 +1,113 @@
 
-const start = document.getElementById("start");
-let timerEl = document.getElementById("time");
-let correct = 0;
-let incorrect = 0;
-const currentQuestion = 0;
-
-var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-
-];
-
-
-
-secondsLeft = 100;
-
-// Timer function and starts questions function
-start.addEventListener("click", function () {
-
-    const timeLeft = setInterval(function () {
-        secondsLeft--;
-        timerEl.textContent = "You have " + secondsLeft + " seconds left.";
-        if (secondsLeft === 0) {
-            clearInterval(timeLeft);
-        }
-    }, 1000)
-    startQuestions();
-});
-
-
-// creates new element with question and button answers
-function startQuestions() {
-    if (startQuestions) {
-        document.getElementById('content').style.visibility = 'hidden';
-    }
-    const questionOne = document.createElement("h2");
-    questionOne.id = 'first';
-    questionOne.textContent = questions[0].title;
-    document.getElementById('first').appendChild(questionOne);
-
-    for (let i = 0; i < questions[0].choices.length; i++) {
-
-        const btn = document.createElement("BUTTON");   // Create a <button> element
-        btn.id = 'button' + i;
-        btn.classList.add('btnList');
-        document.body.appendChild(btn);                 //Append 
-    }
-    //btn.textContent = "Word";                       // Insert text from choices 
-    fillQuestions();
-}
-
-function fillQuestions() {
-    document.getElementById('button0').textContent = questions[currentQuestion].choices[0];
-    document.getElementById('button1').textContent = questions[currentQuestion].choices[1];
-    document.getElementById('button2').textContent = questions[currentQuestion].choices[2];
-    document.getElementById('button3').textContent = questions[currentQuestion].choices[3];
-
-}
-const any = document.getElementsByClassName("btnList");
-any.addEventListener("click", function () {
-    alert('its working');
-});
-
 $(document).ready(function () {
-    $('.btnList').on('click', function () {
+    
+    const start = document.getElementById("start");
+    let timerEl = document.getElementById("time");
+    let fqcontent = document.getElementById('firstquestion');
+    let buttoncontent = document.getElementById('buttoncontent');
+    let realButtonContent = document.getElementById('first');
 
-        if ($(this).text === questions[currentQuestion].answer) {
-            correct++;
-            console.log("correct" + correct);
-        }
-        else {
-            incorrect++;
-            console.log("incorrect" + incorrect);
-        }
+    let currentQuestion = 0;
+    let correct = 0;
+    let incorrect = 0;
+    
+    var questions = [
+        {
+            title: "Commonly used data types DO NOT include:",
+            choices: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts"
+        },
+        {
+            title: "The condition in an if / else statement is enclosed within ____.",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer: "parentheses"
+        },
+        
+    ];
+    
+    
+    secondsLeft = 75;
 
-        // handles score and start next question
-        currentQuestion++;
-        fillQuestions();
+    // Timer function and starts questions function
+    start.addEventListener("click", function () {
+    
+        const timeLeft = setInterval(function () {
+            secondsLeft--;
+            timerEl.textContent = "You have " + secondsLeft + " seconds left.";
+            if (secondsLeft === 0) {
+                clearInterval(timeLeft);
+            }
+        }, 1000)
+        startQuestions();
     });
+
+
+    // creates new element with question and button answers
+    function startQuestions() {
+        if (startQuestions) {
+            document.getElementById('showonstart').textContent = '';
+        }
+        const questionOne = document.createElement("h2");
+        questionOne.textContent = questions[0].title;
+        fqcontent.prepend(questionOne);
+    
+        for (let i = 0; i < questions[0].choices.length; i++) {
+        
+            const btn = document.createElement("BUTTON");   // Create a <button> element
+            btn.id = 'button' + i;
+            btn.classList.add('btnList');
+            realButtonContent.appendChild(btn);
+        }
+        //btn.textContent = "Word";                       // Insert text from choices 
+        fillQuestions();
+    
+    
+    }
+
+    function fillQuestions() {
+        document.getElementById('button0').textContent = questions[currentQuestion].choices[0];
+        document.getElementById('button1').textContent = questions[currentQuestion].choices[1];
+        document.getElementById('button2').textContent = questions[currentQuestion].choices[2];
+        document.getElementById('button3').textContent = questions[currentQuestion].choices[3];
+
+        button0.addEventListener("click", function () {
+            if ($(this).text === questions[0].answer) {
+                correct++;
+            }
+            else {
+                incorrect++;
+            }
+        });
+        button1.addEventListener("click", function () {
+            if ($(this).text === questions[0].answer) {
+                correct++;
+            }
+            else {
+                incorrect++;
+            }
+        });
+        button2.addEventListener("click", function () {
+            if ($(this).text() === questions[0].answer) {
+                correct++;
+            }
+            else {
+                incorrect++;
+            }
+        });
+        button3.addEventListener("click", function () {
+            if ($(this).text === questions[0].answer) {
+                correct++;
+            }
+            else {
+                incorrect++;
+            }
+        });
+    }
 });
+
+
+
+
+
+
+
